d5e8bf2f227b882bde64728036a6369db0315cce
jamesdeperio <deperiojames20@gmail.com>
2018-09-06 11:46:33 +0800

    Fixed:button

--- app/src/main/java/jdp/pocketlib/widget/PocketSpinnerDialog.kt
+++ app/src/main/java/jdp/pocketlib/widget/PocketSpinnerDialog.kt
@@ -76,7 +76,7 @@ class PocketSpinnerDialog<T>(context: Context, private var isFullScreen:Boolean=
 
     private var listener: Listener<T> = object :Listener<T> {
         override fun onItemSelected(selectedObject: T, selectedItem: String, selectedIndex: Int,spinner: Button?) {
-            spinner?.setText(selectedIndex)
+            button?.setText(selectedIndex)
         }
     }
 
@@ -87,7 +87,7 @@ class PocketSpinnerDialog<T>(context: Context, private var isFullScreen:Boolean=
     fun setOnItemSelectedListener(onItemSelectedListener:(selectedObject: T, selectedItem: String, selectedIndex: Int, spinner: Button?)-> Unit) {
         this.listener=object :Listener<T>{
             override fun onItemSelected(selectedObject: T, selectedItem: String, selectedIndex: Int, spinner: Button?) {
-                spinner?.setText(selectedIndex)
+                button?.setText(selectedIndex)
                 onItemSelectedListener(selectedObject,selectedItem,selectedIndex,spinner)
             }
         }
