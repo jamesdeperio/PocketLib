e808d07fcb4962660e3fba118931d1feeea40f10
jamesdeperio <deperiojames20@gmail.com>
2018-09-12 15:16:40 +0800

    Removed: type alias

--- app/src/main/java/jdp/pocketlib/util/EventPublisher.kt
+++ app/src/main/java/jdp/pocketlib/util/EventPublisher.kt
@@ -5,29 +5,31 @@
 package jdp.pocketlib.util
 
 import io.reactivex.disposables.Disposable
+import io.reactivex.functions.Consumer
 import io.reactivex.schedulers.Schedulers
+import io.reactivex.subjects.Subject
 
-class EventPublisher<T : RxSubject>(private val bus: T) {
+class EventPublisher<T : Subject<Any>>(private val bus: T) {
     private lateinit var publisher: MutableMap<String,T>
 
-    fun sendEvent(channel:Channel,event: Event): Unit? {
+    fun sendEvent(channel:String,event: Any): Unit? {
         if (publisher[channel]==null) throw RuntimeException("CHANNEL IS NOT REGISTERED! Please Subscribe.")
         return publisher[channel]!!.onNext(event)
     }
 
-    fun unSubscribeReceiver(channel:Channel) {
+    fun unSubscribeReceiver(channel:String) {
         if (publisher[channel]==null) throw RuntimeException("CHANNEL IS NOT REGISTERED! Please Subscribe.")
         (publisher[channel] as Disposable).dispose()
     }
 
-    fun subscribeReceiver(channel:Channel,eventReceiver: EventReceiver ) {
+    fun subscribeReceiver(channel:String,eventReceiver: Consumer<in Any> ) {
         publisher[channel]  = bus
         publisher[channel]!!.subscribeOn(Schedulers.io())
                 .observeOn(Schedulers.newThread())
                 .subscribe(eventReceiver)
     }
 
-    fun subscribeReceiver(channel: String, eventReceiver: (event:Event) -> Unit) {
+    fun subscribeReceiver(channel: String, eventReceiver: (event:Any) -> Unit) {
         publisher[channel]  = bus
         publisher[channel]!!.subscribeOn(Schedulers.io())
                 .observeOn(Schedulers.newThread())
