13bfd6cc78bd4998cf05d157bbb34a5789f8dbff
jamesdeperio <deperiojames20@gmail.com>
2018-09-13 08:34:34 +0800

    added: mutable map for disposable

--- app/src/main/java/jdp/pocketlib/util/EventPublisher.kt
+++ app/src/main/java/jdp/pocketlib/util/EventPublisher.kt
@@ -12,6 +12,7 @@ import io.reactivex.subjects.*
 @Suppress("UNCHECKED_CAST", "NON_EXHAUSTIVE_WHEN")
 class EventPublisher(private val bus: Bus) {
     private var publisher: MutableMap<String,Any> = HashMap()
+    private var disposable: MutableMap<String,Disposable> = HashMap()
 
     fun sendEvent(channel:String,event: Any): Unit? {
         if (publisher[channel]==null) throw RuntimeException("CHANNEL IS NOT REGISTERED! Please Subscribe.")
@@ -25,24 +26,58 @@ class EventPublisher(private val bus: Bus) {
     }
 
     fun unSubscribeReceiver(channel:String) {
-        if (publisher[channel]==null) throw RuntimeException("CHANNEL IS NOT REGISTERED! Please Subscribe.")
-        (publisher[channel] as Disposable).dispose()
+        if (disposable[channel]==null) throw RuntimeException("CHANNEL IS NOT REGISTERED! Please Subscribe.")
+        disposable[channel]!!.dispose()
     }
 
-    fun subscribeReceiver(channel:String,eventReceiver: Consumer<in Any> ) = when (bus) {
-        Bus.PublishSubject ->   publisher[channel]  = PublishSubject.create<Any>().subscribeOn(Schedulers.io()).observeOn(Schedulers.newThread()).subscribe(eventReceiver)
-        Bus.BehaviorSubject ->  publisher[channel]  = BehaviorSubject.create<Any>().subscribeOn(Schedulers.io()).observeOn(Schedulers.newThread()).subscribe(eventReceiver)
-        Bus.AsyncSubject -> publisher[channel]  = AsyncSubject.create<Any>().subscribeOn(Schedulers.io()).observeOn(Schedulers.newThread()).subscribe(eventReceiver)
-        Bus.ReplaySubject -> publisher[channel]  = ReplaySubject.create<Any>().subscribeOn(Schedulers.io()).observeOn(Schedulers.newThread()).subscribe(eventReceiver)
-        Bus.UnicastSubject ->  publisher[channel]  = UnicastSubject.create<Any>().subscribeOn(Schedulers.io()).observeOn(Schedulers.newThread()).subscribe(eventReceiver)
-   }
+    fun subscribeReceiver(channel:String,eventReceiver: Consumer<in Any> ) {
+        when (bus) {
+            Bus.PublishSubject -> {
+                publisher[channel]  = PublishSubject.create<Any>()
+                disposable[channel]= (publisher[channel]!! as PublishSubject<Any>).subscribeOn(Schedulers.io()).observeOn(Schedulers.newThread()).subscribe(eventReceiver)
+            }
+            Bus.BehaviorSubject -> {
+                publisher[channel]  = BehaviorSubject.create<Any>()
+                disposable[channel]=(publisher[channel]!! as BehaviorSubject<Any>).subscribeOn(Schedulers.io()).observeOn(Schedulers.newThread()).subscribe(eventReceiver)
+            }
+            Bus.AsyncSubject -> {
+                publisher[channel]  = AsyncSubject.create<Any>()
+                disposable[channel]= (publisher[channel]!! as AsyncSubject<Any>).subscribeOn(Schedulers.io()).observeOn(Schedulers.newThread()).subscribe(eventReceiver)
+            }
+            Bus.ReplaySubject -> {
+                publisher[channel]  = ReplaySubject.create<Any>()
+                disposable[channel]= (publisher[channel]!! as ReplaySubject<Any>).subscribeOn(Schedulers.io()).observeOn(Schedulers.newThread()).subscribe(eventReceiver)
+            }
+            Bus.UnicastSubject -> {
+                publisher[channel]  = UnicastSubject.create<Any>()
+                disposable[channel]= (publisher[channel]!! as UnicastSubject<Any>).subscribeOn(Schedulers.io()).observeOn(Schedulers.newThread()).subscribe(eventReceiver)
+            }
+        }
+    }
 
-    fun subscribeReceiver(channel: String, eventReceiver: (event:Any) -> Unit) = when (bus) {
-        Bus.PublishSubject ->   publisher[channel]  = PublishSubject.create<Any>().subscribeOn(Schedulers.io()).observeOn(Schedulers.newThread()).subscribe{ eventReceiver(it)}
-        Bus.BehaviorSubject ->  publisher[channel]  = BehaviorSubject.create<Any>().subscribeOn(Schedulers.io()).observeOn(Schedulers.newThread()).subscribe{ eventReceiver(it)}
-        Bus.AsyncSubject -> publisher[channel]  = AsyncSubject.create<Any>().subscribeOn(Schedulers.io()).observeOn(Schedulers.newThread()).subscribe{ eventReceiver(it)}
-        Bus.ReplaySubject -> publisher[channel]  = ReplaySubject.create<Any>().subscribeOn(Schedulers.io()).observeOn(Schedulers.newThread()).subscribe{ eventReceiver(it)}
-        Bus.UnicastSubject ->  publisher[channel]  = UnicastSubject.create<Any>().subscribeOn(Schedulers.io()).observeOn(Schedulers.newThread()).subscribe{ eventReceiver(it)}
+    fun subscribeReceiver(channel: String, eventReceiver: (event:Any) -> Unit){
+        when (bus) {
+            Bus.PublishSubject -> {
+                publisher[channel]  = PublishSubject.create<Any>()
+                (publisher[channel]!! as PublishSubject<Any>).subscribeOn(Schedulers.io()).observeOn(Schedulers.newThread()).subscribe{ eventReceiver(it) }
+            }
+            Bus.BehaviorSubject -> {
+                publisher[channel]  = BehaviorSubject.create<Any>()
+                (publisher[channel]!! as BehaviorSubject<Any>).subscribeOn(Schedulers.io()).observeOn(Schedulers.newThread()).subscribe{ eventReceiver(it) }
+            }
+            Bus.AsyncSubject -> {
+                publisher[channel]  = AsyncSubject.create<Any>()
+                (publisher[channel]!! as AsyncSubject<Any>).subscribeOn(Schedulers.io()).observeOn(Schedulers.newThread()).subscribe{ eventReceiver(it) }
+            }
+            Bus.ReplaySubject -> {
+                publisher[channel]  = ReplaySubject.create<Any>()
+                (publisher[channel]!! as ReplaySubject<Any>).subscribeOn(Schedulers.io()).observeOn(Schedulers.newThread()).subscribe{ eventReceiver(it) }
+            }
+            Bus.UnicastSubject -> {
+                publisher[channel]  = UnicastSubject.create<Any>()
+                (publisher[channel]!! as UnicastSubject<Any>).subscribeOn(Schedulers.io()).observeOn(Schedulers.newThread()).subscribe{ eventReceiver(it) }
+            }
+        }
     }
 
 }
