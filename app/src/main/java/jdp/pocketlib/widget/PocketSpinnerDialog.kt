9034a2f5b38d8824c2e4ebb5f14bcef3a0ba70e3
jamesdeperio <deperiojames20@gmail.com>
2018-10-19 11:51:39 +0800

    FIXED: bug in adding items in pocketspinnerdialog

--- app/src/main/java/jdp/pocketlib/widget/PocketSpinnerDialog.kt
+++ app/src/main/java/jdp/pocketlib/widget/PocketSpinnerDialog.kt
@@ -125,10 +125,8 @@ class PocketSpinnerDialog<T>(context: Context, private var isFullScreen:Boolean=
     }
 
     fun addItem (items:MutableMap<T,String>): PocketSpinnerDialog<T> {
-        var x=0
         items.forEach {
-            adapter.itemList[adapter.itemList.size+x]= PocketSpinnerItem(item = it.key,itemString = it.value )
-            x++
+            adapter.itemList[adapter.itemList.size]= PocketSpinnerItem(item = it.key,itemString = it.value )
         }
         adapter.searchItemList=adapter.itemList
         adapter.notifyDataSetChanged()
@@ -137,7 +135,7 @@ class PocketSpinnerDialog<T>(context: Context, private var isFullScreen:Boolean=
 
     fun addItem (items:MutableList<T>): PocketSpinnerDialog<T> {
         items.withIndex().forEach {
-            adapter.itemList[adapter.itemList.size+it.index]= PocketSpinnerItem(item = it.value ,itemString = it.value.toString() )
+            adapter.itemList[adapter.itemList.size]= PocketSpinnerItem(item = it.value ,itemString = it.value.toString() )
         }
         adapter.searchItemList=adapter.itemList
         adapter.notifyDataSetChanged()
@@ -147,7 +145,7 @@ class PocketSpinnerDialog<T>(context: Context, private var isFullScreen:Boolean=
     fun addItem (itemObject:MutableList<T>,itemString:MutableList<String>): PocketSpinnerDialog<T> {
         if (itemObject.size!=itemString.size) throw RuntimeException("objectlist and itemlist did not match!")
         itemObject.withIndex().forEach {
-            adapter.itemList[adapter.itemList.size+it.index]= PocketSpinnerItem(item = it.value ,itemString = itemString[it.index] )
+            adapter.itemList[adapter.itemList.size]= PocketSpinnerItem(item = it.value ,itemString = itemString[it.index] )
         }
         adapter.searchItemList=adapter.itemList
         adapter.notifyDataSetChanged()
