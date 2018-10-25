7a93cba7f494e1fb619f3216ccd87baa96430c3e
jamesdeperio <deperiojames20@gmail.com>
2018-10-25 15:29:04 +0800

    ADDED: Additional methods

--- app/src/main/java/jdp/pocketlib/widget/PocketSpinner.kt
+++ app/src/main/java/jdp/pocketlib/widget/PocketSpinner.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Color
 import android.graphics.Typeface
 import android.support.design.widget.TextInputEditText
+import android.support.design.widget.TextInputLayout
 import android.util.AttributeSet
 import android.view.Gravity
 import android.view.LayoutInflater
@@ -14,20 +15,44 @@ import jdp.pocketlib.R
 
 
 class PocketSpinner : FrameLayout {
-
-    var hint:String? ="Hint"
-    var text:String? =""
-    var textColor:Int =Color.BLACK
-    var hintColor:Int =Color.BLACK
-    var bgColor:Int =Color.BLACK
-    var gravity:Int =Gravity.START
-    private  var layout: View
+    private var hint:String? ="Hint"
+    private var text:String? =""
+    private var textColor:Int =Color.BLACK
+    private var hintColor:Int =Color.BLACK
+    private var bgColor:Int =Color.BLACK
+    private var gravity:Int =Gravity.START
+    private var hintGravity:Int =Gravity.START
+    private var layout: View
 
     fun setFontFamilyFromAsset(fontPath:String): PocketSpinner {
         val font = Typeface.createFromAsset(resources.assets, fontPath)
         layout.findViewById<TextInputEditText>(R.id.textbox).typeface = font
+        layout.findViewById<TextInputLayout>(R.id.textboxLayout).typeface = font
         return this
     }
+
+    fun setHint(hint:String): PocketSpinner {
+        layout.findViewById<TextInputLayout>(R.id.textboxLayout).hint = hint
+        return this
+    }
+    fun setHintColor(color:Int): PocketSpinner {
+        layout.findViewById<TextInputEditText>(R.id.textbox).setHintTextColor(color)
+        return this
+    }
+    fun setTextColor(color:Int): PocketSpinner {
+        layout.findViewById<TextInputEditText>(R.id.textbox).setTextColor(color)
+        return this
+    }
+
+    fun setHintGravity(gravity:Int): PocketSpinner {
+        layout.findViewById<TextInputLayout>(R.id.textboxLayout).gravity = gravity
+        return this
+    }
+    fun setGravity(gravity:Int): PocketSpinner {
+        layout.findViewById<TextInputEditText>(R.id.textbox).gravity = gravity
+        return this
+    }
+
     constructor(context: Context): super(context) {
         layout  = (context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater).inflate(R.layout.view_pocketspinner, this)
         if (!isInEditMode)    setup(null)
@@ -53,11 +78,11 @@ class PocketSpinner : FrameLayout {
             bgColor = attrib.getColor(R.styleable.PocketSpinner_pkt_background_color,Color.WHITE)
             hintColor = attrib.getColor(R.styleable.PocketSpinner_pkt_hint_color,Color.BLUE)
             gravity = attrib.getInt(R.styleable.PocketSpinner_pkt_gravity,0)
+            hintGravity = attrib.getInt(R.styleable.PocketSpinner_pkt_hint_gravity,0)
             attrib.recycle()
         }
         layout.findViewById<TextInputEditText>(R.id.textbox).apply {
             this.setText(this@PocketSpinner.text?:"")
-            this.hint= this@PocketSpinner.hint?:""
             this.setHintTextColor(hintColor)
             this.setTextColor(textColor)
             this.gravity = when(this@PocketSpinner.gravity){
@@ -67,9 +92,18 @@ class PocketSpinner : FrameLayout {
                 else -> Gravity.START
             }
         }
-        layout.findViewById<FrameLayout>(R.id.textboxLayout).apply {
-            this.setBackgroundColor(this@PocketSpinner.bgColor)
+        layout.findViewById<TextInputLayout>(R.id.textboxLayout).apply {
+            this.hint=this@PocketSpinner.hint?:""
+            this.gravity = when(this@PocketSpinner.gravity){
+                0-> Gravity.START
+                1-> Gravity.END
+                2-> Gravity.TOP
+                else -> Gravity.START
+            }
         }
+
+        setBackgroundColor(this@PocketSpinner.bgColor)
+
         layout.findViewById<ImageView>(R.id.textboxImg).apply {
             this.setBackgroundColor(this@PocketSpinner.bgColor)
         }
