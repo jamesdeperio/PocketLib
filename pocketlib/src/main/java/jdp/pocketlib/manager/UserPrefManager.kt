85251e7984267973bcb12fca726936ccc8825c1a
jamesdeperio <deperiojames20@gmail.com>
2019-01-29 14:38:22 +0800

    Updated: user pref secure data by encrypting it using AES

--- pocketlib/src/main/java/jdp/pocketlib/manager/UserPrefManager.kt
+++ pocketlib/src/main/java/jdp/pocketlib/manager/UserPrefManager.kt
@@ -2,22 +2,24 @@ package jdp.pocketlib.manager
 import android.content.Context
 import android.content.SharedPreferences
 import com.google.gson.Gson
+import jdp.pocketlib.util.AESUtil
 
-class UserPrefManager( val context: Context) {
+class UserPrefManager( val context: Context, key: String) {
     var prefList:MutableMap<String,SharedPreferences> = HashMap()
+    var aes = AESUtil(key)
     @Synchronized
     inline fun <reified  T> set( key:String,value: T): UserPrefManager {
         if (prefList.none { it.key==key+"|"+T::class.java.simpleName }) {
             prefList[key+"|"+T::class.java.simpleName] = context.getSharedPreferences(T::class.java.simpleName, Context.MODE_PRIVATE)
         }
         val prefsEditor:SharedPreferences.Editor = prefList[key+"|"+T::class.java.simpleName]!!.edit()
-        prefsEditor.putString(key,  Gson().toJson(value))
+        prefsEditor.putString(key,  aes.encrypt(Gson().toJson(value)))
         prefsEditor.apply()
         return this
     }
     @Synchronized
     inline fun <reified T> get(key: String):T {
         val json = prefList[key+"|"+T::class.java.simpleName]!!.getString(key, "")!!
-        return Gson().fromJson<T>(json, T::class.java)
+        return Gson().fromJson<T>(aes.decrypt(json), T::class.java)
     }
 }
---
+++ pocketlib/src/main/java/jdp/pocketlib/util/AESUtil.kt
@@ -0,0 +1,66 @@
+package jdp.pocketlib.util
+
+import android.annotation.SuppressLint
+import javax.crypto.Cipher
+import javax.crypto.spec.SecretKeySpec
+
+class AESUtil(private var keyValue:String="secure_key") {
+    private var hex=" 0123456789ABCDEF"
+
+    @Throws(Exception::class)
+    fun encrypt(cleartext: String): String {
+        val key = SecretKeySpec(byteArrayOf(keyValue.toByte()), "AES")
+        val result = encrypt(key.encoded, cleartext.toByteArray())
+        return toHex(result)
+    }
+
+    @Throws(Exception::class)
+    fun decrypt(encrypted: String): String {
+        val enc = toByte(encrypted)
+        val result = decrypt(enc)
+        return String(result)
+    }
+
+    @Throws(Exception::class)
+    private fun encrypt(raw: ByteArray, clear: ByteArray): ByteArray {
+        val skeySpec = SecretKeySpec(raw, "AES")
+        val cipher = Cipher.getInstance("AES")
+        cipher.init(Cipher.ENCRYPT_MODE, skeySpec)
+        return cipher.doFinal(clear)
+    }
+
+    @SuppressLint("GetInstance")
+    @Throws(Exception::class)
+    private fun decrypt(encrypted: ByteArray): ByteArray {
+        val skeySpec = SecretKeySpec(byteArrayOf(keyValue.toByte()), "AES")
+        val cipher = Cipher.getInstance("AES")
+        cipher.init(Cipher.DECRYPT_MODE, skeySpec)
+        return cipher.doFinal(encrypted)
+    }
+
+    private fun toByte(hexString: String): ByteArray {
+        val len = hexString.length / 2
+        val result = ByteArray(len)
+        for (i in 0 until len)
+            result[i] = Integer.valueOf(
+                hexString.substring(2 * i, 2 * i + 2),
+                16
+            ).toByte()
+        return result
+    }
+
+    private fun toHex(buf: ByteArray?): String {
+        if (buf == null)
+            return ""
+        val result = StringBuffer(2 * buf.size)
+        for (i in buf.indices) {
+            appendHex(result, buf[i])
+        }
+        return result.toString()
+    }
+
+    private fun appendHex(sb: StringBuffer, b: Byte) {
+        sb.append(hex[(b.toInt() shr 4) and 0x0f])
+        sb.append(hex[b.toInt()  and 0x0f])
+    }
+}
