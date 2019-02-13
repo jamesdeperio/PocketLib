dcdebed90c8c3fb48cc61d0bdb765d587b1f4ef2
jamesdeperio <deperiojames20@gmail.com>
2019-02-13 10:12:40 +0800

    Added: FragmentManager.useGallery() Added: FragmentManager.useMultiSelectGallery() Added: FragmentManager.useCamera()

--- pocketlib/src/main/java/jdp/pocketlib/ext/FragmentManagerExt.kt
+++ pocketlib/src/main/java/jdp/pocketlib/ext/FragmentManagerExt.kt
@@ -2,9 +2,14 @@
 
 package jdp.pocketlib.ext
 
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
+import android.support.annotation.RequiresApi
 import android.support.v4.app.FragmentManager
 import jdp.pocketlib.builder.PageBuilder
 import jdp.pocketlib.util.FragmentTransaction
+import jdp.pocketlib.util.RxImageRequest
 
 inline fun FragmentManager.add(allowStateLoss:Boolean=false,isAnimationEnabled: Boolean = true,properties: FragmentTransaction.() -> Unit) {
     FragmentTransaction.add(this, allowStateLoss,isAnimationEnabled,properties)
@@ -16,5 +21,18 @@ inline fun FragmentManager.replace(allowStateLoss:Boolean=false,isAnimationEnabl
 
 inline fun FragmentManager.createViewPager(properties: PageBuilder.() -> Unit) {
     PageBuilder.build(this,properties)
+}
+
+inline fun FragmentManager.useGallery(noinline callback:(bitmap:Bitmap) -> Unit) {
+    RxImageRequest.request(RxImageRequest.GALLERY,this,callback)
+}
+
+inline fun FragmentManager.useCamera(noinline callback:(bitmap:Bitmap) -> Unit) {
+    RxImageRequest.request(RxImageRequest.CAMERA,this,callback)
+}
+
+@RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
+inline fun FragmentManager.useMultipleSelectGallery(noinline callback:(uris:ArrayList<Uri>) -> Unit) {
+    RxImageRequest.requestMultipleImage(this,callback)
 }
 
