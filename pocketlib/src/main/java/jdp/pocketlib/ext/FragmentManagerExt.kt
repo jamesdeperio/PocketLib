3f744465a912c002caddc76281c0214e3a73edec
jamesdeperio <deperiojames20@gmail.com>
2019-02-13 11:31:04 +0800

    Added: RxPermissionRequest Added: FragmentManager.requestPermissions()

--- pocketlib/src/main/java/jdp/pocketlib/ext/FragmentManagerExt.kt
+++ pocketlib/src/main/java/jdp/pocketlib/ext/FragmentManagerExt.kt
@@ -9,7 +9,9 @@ import android.support.annotation.RequiresApi
 import android.support.v4.app.FragmentManager
 import jdp.pocketlib.builder.PageBuilder
 import jdp.pocketlib.util.FragmentTransaction
+import jdp.pocketlib.util.PermissionResult
 import jdp.pocketlib.util.RxImageRequest
+import jdp.pocketlib.util.RxPermissionRequest
 
 inline fun FragmentManager.add(allowStateLoss:Boolean=false,isAnimationEnabled: Boolean = true,properties: FragmentTransaction.() -> Unit) {
     FragmentTransaction.add(this, allowStateLoss,isAnimationEnabled,properties)
@@ -34,5 +36,9 @@ inline fun FragmentManager.useCamera(noinline callback:(bitmap:Bitmap) -> Unit) 
 @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
 inline fun FragmentManager.useMultipleSelectGallery(noinline callback:(uris:ArrayList<Uri>) -> Unit) {
     RxImageRequest.requestMultipleImage(this,callback)
+}
+
+inline fun FragmentManager.requestPermissions(vararg permission: String, noinline callback:(result:PermissionResult) -> Unit) {
+    RxPermissionRequest.requestPermission(permissions = *permission,fragmentManager = this,callback = callback)
 }
 
---
+++ pocketlib/src/main/java/jdp/pocketlib/util/RxPermissionRequest.kt
@@ -0,0 +1,45 @@
+package jdp.pocketlib.util
+
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import jdp.pocketlib.base.BaseFragment
+import jdp.pocketlib.manager.Bus
+import jdp.pocketlib.manager.EventBusManager
+
+
+
+@SuppressLint("ValidFragment")
+private class TempPermissionFragment(private val eventBusManager: EventBusManager,  vararg val permissions: String) :BaseFragment() {
+    override fun onInitialization(savedInstanceState: Bundle?) {
+        requestPermissions(permissions,1)
+    }
+    override fun onViewDidLoad(savedInstanceState: Bundle?) {}
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        (0 until grantResults.size).forEach { i ->
+            permissions.filter {  (permissions[i]==it) }
+                .forEach { eventBusManager.sendEvent(RxPermissionRequest.PERMISSION_REQUEST,PermissionResult(it,true)) }
+        }
+    }
+}
+ data class PermissionResult( var name:String="", var result:Boolean=false)
+object RxPermissionRequest {
+    const val PERMISSION_REQUEST="PERMISSION_REQUEST"
+    fun requestPermission(vararg permissions:String,fragmentManager:FragmentManager,callback:(bitmap:PermissionResult)-> Unit) {
+        val eventBusManager= EventBusManager(Bus.PublishSubject)
+        val tempFragment :Fragment= TempPermissionFragment(eventBusManager, *permissions)
+        fragmentManager.beginTransaction()
+            .add(tempFragment, tempFragment.javaClass.simpleName)
+            .commit()
+        eventBusManager.subscribeReceiver(PERMISSION_REQUEST) {
+            callback(it as PermissionResult)
+            fragmentManager.beginTransaction()
+                .remove(tempFragment)
+                .commit()
+            eventBusManager.disposeAllChannel()
+        }
+    }
+}
